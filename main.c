843eaca8aae5cfb619e5d64f0f0975efaee757d4
yorjjeartemitt <a06298990@email.com>
2026-07-14 14:08:03 +0300

    add network parser

--- main.c
+++ main.c
@@ -688,7 +688,7 @@ static void activate(GtkApplication *app, gpointer data){
 }
 
 int main(int argc,char *argv[]){
-	GtkApplication *app=gtk_application_new("org.siem.app",G_APPLICATION_DEFAULT_FLAGS);
+	GtkApplication *app=gtk_application_new("org.siem.app",G_APPLICATION_FLAGS_NONE);
 	g_signal_connect(app,"activate",G_CALLBACK(activate),NULL);
 	int status=g_application_run(G_APPLICATION(app),argc,argv);
 	g_object_unref(app);
